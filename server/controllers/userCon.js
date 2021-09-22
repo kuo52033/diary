664105e699b49cbf12060aeed38630822dc56834
Tim Kuo <tim31422@gmail.com>
2021-09-22 18:11:40 +0800

    修改cookie3

--- server/controllers/userCon.js
+++ server/controllers/userCon.js
@@ -62,11 +62,13 @@ export const userSignin = async (req, res) => {
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
+      secure: true,
       sameSite: "None",
       maxAge: 31557600000,
     });
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
+      secure: true,
       sameSite: "None",
       maxAge: 31557600000,
     });
